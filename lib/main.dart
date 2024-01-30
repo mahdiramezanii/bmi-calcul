c8d8da4e9ac934dd2770311245a866255c259c4b
mahdiramazani <mahdiramazani1281@gmail.com>
2024-01-30 16:36:37 +0330

    fix error

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-          body: Center(
-              child: DecoratedBox(
-        position: DecorationPosition.background,
-        decoration: BoxDecoration(
-            color: Colors.red, borderRadius: BorderRadius.only(topLeft: Radius.circular(50))),
-        child: SizedBox(
-          height: 100,
-          width: 100,
-        ),
-      ))),
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0,500,0,0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        width: 200,
+                        height: 30,
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            bottomLeft: Radius.circular(20)
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+              
+                  Row(
+                    children: [
+                      Container(
+                        width: 200,
+                        height: 30,
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(20),
+                            bottomRight: Radius.circular(20),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
